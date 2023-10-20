1100d82d05ce35630e2bbea91b87b6336184afdd
lch <jamsuham75@naver.com>
2023-10-20 11:48:27 +0900

    쿠폰 업데이트

--- src/test.js
+++ src/test.js
@@ -1,25 +1,3 @@
 console.log("오늘 마음에 들어");
 console.log("코드 추가");
-
-let data = [
-    {
-      "id" : 0,
-      "title" : "white and Black",
-      "content" : "Born in France",
-      "price" : 120000
-    },
-    {
-      "id" : 1,
-      "title" : "Red Knit",
-      "content" : "Born in Seoul",
-      "price" : 110000
-    },
-    {
-      "id" : 2,
-      "title" : "Grey Yordan",
-      "content" : "Born in States",
-      "price" : 110000
-    }
-  ];
-
-export default data;
+console.log("쿠폰 발행");
